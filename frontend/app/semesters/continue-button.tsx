800d1667a7938b6e42bfdf1687f8c992c4cc8ce1
Simon Otterbeck <simon.otterbeck@protonmail.com>
2024-06-28 19:06:16 +0200

    fix(frontend): add missing Suspense boundary to ContinueButton

--- frontend/app/semesters/continue-button.tsx
+++ frontend/app/semesters/continue-button.tsx
@@ -3,12 +3,23 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 import { twMerge } from 'tailwind-merge';
+import React, { Suspense } from 'react';
 
 export interface ContinueButtonProps {
   className?: string;
 }
 
 export default function ContinueButton({
+  className,
+}: Readonly<ContinueButtonProps>) {
+  return (
+    <Suspense>
+      <ContinueButtonWithParams className={className} />
+    </Suspense>
+  );
+}
+
+function ContinueButtonWithParams({
   className,
 }: Readonly<ContinueButtonProps>) {
   const params = useSearchParams();
