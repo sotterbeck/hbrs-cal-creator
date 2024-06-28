4a247be57b9f9bf958368c8d6d619511946b87bd
Simon Otterbeck <simon.otterbeck@protonmail.com>
2024-06-28 21:21:32 +0200

    build(backend): add spring boot actuator

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
