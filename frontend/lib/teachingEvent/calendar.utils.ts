d75dcacc6537b90f6e3b89244c557e4acf3bd0a9
Tobias Holzapfel <123665093+Tobi11001@users.noreply.github.com>
2025-04-01 18:29:54 +0200

    positioning events in calendar grid

--- frontend/lib/teachingEvent/calendar.utils.ts
+++ frontend/lib/teachingEvent/calendar.utils.ts
@@ -7,3 +7,21 @@ export function getTimeIntervals() {
     }
     return timeIntervals;
 }
+
+export function getEventPosition(event: EventModel) {
+    const start = [+event.startTime.split(":")[0], +event.startTime.split(":")[1]];
+    const end = [+event.endTime.split(":")[0], +event.endTime.split(":")[1]];
+    //First start is 8 | 1 hour is divided in 4 rows | first row for events is 2
+    const rowStart = (((start[0] - 8) * 4) + (start[1] / 15) + 2)
+    //Difference between start and end
+    const rowSpan = ((end[0] - 8) * 4 + (end[1] / 15)+2) - rowStart
+    const weekdayToColMap: { [index: string]: any } = {
+        Mo: 2,
+        Di: 3,
+        Mi: 4,
+        Do: 5,
+        Fr: 6
+    }
+    const colStart = weekdayToColMap[event.day];
+    return {rowStart, rowSpan, colStart}
+}
