2dfe2b44557782f7eb0f00fa682a9e4714f88fd3
Simon Otterbeck <simon.otterbeck@protonmail.com>
2025-09-16 18:50:45 +0200

    fix(frontend): add default card style fallback in CalendarEventCard

--- frontend/components/teachingEvents/calendar/event-card.tsx
+++ frontend/components/teachingEvents/calendar/event-card.tsx
@@ -6,20 +6,22 @@ interface CalendarEventCardProps {
 
 export default function CalendarEventCard(props: CalendarEventCardProps) {
   return (
-      <div
-        className={`text-wrap min-w-full max-w-min md:w-auto h-full rounded p-2 ${cardStyleLookup[getEventCardStyle(props.event.types)]}`}
-      >
-        <p className={'text-xs font-bold'}>{props.event.title}</p>
-        <p className={'text-xs'}>
-          {props.event.startTime} - {props.event.endTime}
-        </p>
-        <p className={'text-xs'}>{props.event.room}</p>
-        <p className={'text-xs'}>{props.event.instructor}</p>
-      </div>
+    <div
+      className={`h-full min-w-full max-w-min text-wrap rounded p-2 md:w-auto ${cardStyleLookup[getEventCardStyle(props.event.types)] ?? cardStyleLookup['default']}`}
+    >
+      <p className={'text-xs font-bold'}>{props.event.title}</p>
+      <p className={'text-xs'}>
+        {props.event.startTime} - {props.event.endTime}
+      </p>
+      <p className={'text-xs'}>{props.event.room}</p>
+      <p className={'text-xs'}>{props.event.instructor}</p>
+    </div>
   );
 }
 
 export const cardStyleLookup: { [index: string]: string } = {
+  default:
+    'border-l-4 border-l-lime-950 bg-lime-400 text-lime-950 dark:border-l-lime-400 dark:bg-lime-950 dark:text-lime-400',
   v: 'border-l-4 border-l-teal-950 bg-teal-400 text-teal-950 dark:border-l-teal-400 dark:bg-teal-950 dark:text-teal-400',
   ue: 'border-l-4 border-l-pink-950 bg-pink-400 text-pink-950 dark:border-l-pink-400 dark:bg-pink-950 dark:text-pink-400',
   p: 'border-l-4 border-l-sky-950 bg-sky-400 text-sky-950 dark:border-l-sky-400 dark:bg-sky-950 dark:text-sky-400',
